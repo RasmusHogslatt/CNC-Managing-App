89c8fe4e4fed4d72055f356d671d57bfe12fc194
RasmusHogslatt <r.hogslatt@gmail.com>
2024-04-01 14:43:12 +0200

    test a

--- assets/sw.js
+++ assets/sw.js
@@ -16,10 +16,10 @@ self.addEventListener('install', function (e) {
 });
 
 /* Serve cached content when offline */
-// self.addEventListener('fetch', function (e) {
-//   e.respondWith(
-//     caches.match(e.request).then(function (response) {
-//       return response || fetch(e.request);
-//     })
-//   );
-// });
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
