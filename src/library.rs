0eb12835474f623fb4633234e775516007586ac6
RasmusHogslatt <r.hogslatt@gmail.com>
2024-04-03 12:47:15 +0200

    Added scrollareas to library selection and display

--- src/library.rs
+++ src/library.rs
@@ -1,3 +1,6 @@
+use egui::scroll_area::ScrollBarVisibility;
+use egui::ScrollArea;
+
 use crate::adapter::*;
 
 use crate::holder::*;
@@ -20,40 +23,42 @@ impl Library {
         let mut is_window_open = true;
         egui::Window::new("Library")
             .open(&mut is_window_open)
-            .vscroll(true)
             .show(ctx, |ui| {
                 ui.horizontal(|ui| {
                     ui.radio_value(&mut self.category, MagazineContentType::Tool, "Tool");
                     ui.radio_value(&mut self.category, MagazineContentType::Holder, "Holder");
                     ui.radio_value(&mut self.category, MagazineContentType::Adapter, "Adapter");
                 });
-                match self.category {
-                    MagazineContentType::Tool => {
-                        for (index, tool) in self.tools.iter_mut().enumerate() {
-                            ui.horizontal(|ui| {
-                                ui.label(format!("Tool {}: ", index));
-                                ui.separator();
-                                tool.display(ui);
-                            });
+                ScrollArea::vertical()
+                    .auto_shrink(false)
+                    .scroll_bar_visibility(ScrollBarVisibility::AlwaysVisible)
+                    .show(ui, |ui| match self.category {
+                        MagazineContentType::Tool => {
+                            for (index, tool) in self.tools.iter_mut().enumerate() {
+                                ui.horizontal(|ui| {
+                                    ui.label(format!("Tool {}: ", index));
+                                    ui.separator();
+                                    tool.display(ui);
+                                });
+                            }
                         }
-                    }
-                    MagazineContentType::Holder => {
-                        for (index, holder) in self.holders.iter_mut().enumerate() {
-                            ui.horizontal(|ui| {
-                                ui.label(format!("Holder {}: ", index));
-                                holder.display(ui);
-                            });
+                        MagazineContentType::Holder => {
+                            for (index, holder) in self.holders.iter_mut().enumerate() {
+                                ui.horizontal(|ui| {
+                                    ui.label(format!("Holder {}: ", index));
+                                    holder.display(ui);
+                                });
+                            }
                         }
-                    }
-                    MagazineContentType::Adapter => {
-                        for (index, adapter) in self.adapters.iter_mut().enumerate() {
-                            ui.horizontal(|ui| {
-                                ui.label(format!("Adapter {}: ", index));
-                                adapter.display(ui);
-                            });
+                        MagazineContentType::Adapter => {
+                            for (index, adapter) in self.adapters.iter_mut().enumerate() {
+                                ui.horizontal(|ui| {
+                                    ui.label(format!("Adapter {}: ", index));
+                                    adapter.display(ui);
+                                });
+                            }
                         }
-                    }
-                }
+                    });
             });
         is_window_open
     }
@@ -135,12 +140,16 @@ pub fn select_tool_from_library(app: &mut ManagingApp, ctx: &egui::Context) {
     egui::Window::new("Select tool from library")
         .open(&mut is_window_open)
         .show(ctx, |ui| {
-            for (i, tool) in app.library.tools.iter().enumerate() {
-                tool.display(ui);
-                if ui.button(format!("Move")).clicked() {
-                    app.move_selections.selected_tool_index_library = Some(i);
-                }
-            }
+            egui::ScrollArea::vertical()
+                .scroll_bar_visibility(ScrollBarVisibility::AlwaysVisible)
+                .show(ui, |ui| {
+                    for (i, tool) in app.library.tools.iter().enumerate() {
+                        tool.display(ui);
+                        if ui.button(format!("Move")).clicked() {
+                            app.move_selections.selected_tool_index_library = Some(i);
+                        }
+                    }
+                });
         });
     if !is_window_open {
         app.app_states.move_state = None;
@@ -223,12 +232,16 @@ pub fn select_holder_from_library(app: &mut ManagingApp, ctx: &egui::Context) {
     egui::Window::new("Select holder from library")
         .open(&mut is_window_open)
         .show(ctx, |ui| {
-            for (i, holder) in app.library.holders.iter().enumerate() {
-                holder.display(ui);
-                if ui.button(format!("Move")).clicked() {
-                    app.move_selections.selected_holder_index_library = Some(i);
-                }
-            }
+            egui::ScrollArea::vertical()
+                .scroll_bar_visibility(ScrollBarVisibility::AlwaysVisible)
+                .show(ui, |ui| {
+                    for (i, holder) in app.library.holders.iter().enumerate() {
+                        holder.display(ui);
+                        if ui.button(format!("Move")).clicked() {
+                            app.move_selections.selected_holder_index_library = Some(i);
+                        }
+                    }
+                });
         });
     if !is_window_open {
         app.app_states.move_state = None;
@@ -311,12 +324,16 @@ pub fn select_adapter_from_library(app: &mut ManagingApp, ctx: &egui::Context) {
     egui::Window::new("Select adapter from library")
         .open(&mut is_window_open)
         .show(ctx, |ui| {
-            for (i, adapter) in app.library.adapters.iter().enumerate() {
-                adapter.display(ui);
-                if ui.button(format!("Move")).clicked() {
-                    app.move_selections.selected_adapter_index_library = Some(i);
-                }
-            }
+            egui::ScrollArea::vertical()
+                .scroll_bar_visibility(ScrollBarVisibility::AlwaysVisible)
+                .show(ui, |ui| {
+                    for (i, adapter) in app.library.adapters.iter().enumerate() {
+                        adapter.display(ui);
+                        if ui.button(format!("Move")).clicked() {
+                            app.move_selections.selected_adapter_index_library = Some(i);
+                        }
+                    }
+                });
         });
     if !is_window_open {
         app.app_states.move_state = None;
